7a55c1ce42ea649d17860128d2d3525eb88284c7
sandip rai <iamsandip-git>
2025-03-27 09:35:13 +0545

    edit github action

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-n4n7*wxl8ez1yto@su&d26crf#a+h4n3i7(z7l5xt3(5odqh4y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.8.143.101"]
 
 
 # Application definition
