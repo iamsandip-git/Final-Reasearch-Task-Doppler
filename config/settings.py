f1f3282763c21717d244b5143ec22a5debf25cbc
sandip rai <iamsandip-git>
2025-03-28 16:55:38 +0545

    edit github action

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # print("Allowed hosts is:")
 # print(os.getenv("ALLOWED_HOSTS"))
 
-ALLOWED_HOSTS = ["18.132.37.204"]
+ALLOWED_HOSTS = ["18.132.37.204", "localhost"]
 
 
 # Application definition
