83192efc76917e077c63c22c3bbd2203b6ad9d91
sandip rai <iamsandip-git>
2025-03-29 16:26:36 +0545

    edit github action

--- frontend/news-site/src/features/newsApi.js
+++ frontend/news-site/src/features/newsApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://18.132.37.204:8000';
+const API_BASE_URL = 'http://13.40.66.100:8000';
 
 export const fetchAllNews = async (page = 1) => {
   try {
