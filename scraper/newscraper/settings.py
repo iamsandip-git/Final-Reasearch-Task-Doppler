fcfc63a57bf0b557799ee438644e1d1a8baa3d2e
sandip rai <iamsandip-git>
2025-03-31 14:44:23 +0545

    edit github action

--- scraper/newscraper/settings.py
+++ scraper/newscraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "newscraper.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "newscraper (+http://www.yourdomain.com)"
+USER_AGENT = "PostmanRuntime/7.36.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
